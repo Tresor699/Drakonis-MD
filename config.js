eacaa5b56c8e4ee7727ea545b8fe587909115280
Tresor699 <skillerhack700@gmail.com>
2025-07-31 10:06:34 +0100

    Update config.js
    drakonis~5nsRRIba#ZYI3f2JICOQ41xhGHG8wOJHiRuGkSnh-dL4iDwUM-20

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with 
-  OWNER_NUMBER: "1(236)362-1958", // PUT OWNER NUMBER HERE
+  SESSION_ID: " ",  // drakonis~5nsRRIba#ZYI3f2JICOQ41xhGHG8wOJHiRuGkSnh-dL4iDwUM-20
+  OWNER_NUMBER: "1(236)362-1958", // 243835861275
   PREFIX: "." // prefix (e.g., ., /, !, *)
-};
+};.
